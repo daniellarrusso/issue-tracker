0fb7376417fc73a0ddf98a8a175d49849a7d4ba4
Nathan Adams <npadams21@gmail.com>
2025-03-20 17:22:05 +0000

    refactor: split issue details into smaller comps

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,12 +1,9 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import { prisma } from '@/prisma/client';
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
+import { Box, Grid } from '@radix-ui/themes';
 import delay from 'delay';
 import { notFound } from 'next/navigation';
-import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import Link from 'next/link';
+import EditIssueButton from './EditIssuePage';
+import IssueDetails from './IssueDetails';
 
 interface Props {
   params: { id: string; };
@@ -25,17 +22,10 @@ const IssueDetailPage = async ({ params }: Props) => {
   return (
     <Grid gap={'5'} columns={{ initial: '1', md: '2' }}>
       <Box>
-        <Heading>{issue.title}</Heading>
-        <Flex gap={'3'} mb={'2'}>
-          <IssueStatusBadge status={issue.status} />
-          <Text>{issue.createdAt.toDateString()}</Text>
-        </Flex>
-        <Card className='prose mt-4'>
-          <ReactMarkdown>{issue.description}</ReactMarkdown>
-        </Card>
+        <IssueDetails issue={issue} />
       </Box>
       <Box>
-        <Button> <Pencil2Icon /> <Link href={`/issuses/${issue.id}/edit`}>Edit Issue</Link> </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   );
