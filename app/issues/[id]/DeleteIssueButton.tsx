93e7359abe754e10c93b058ac8cce73ee4486747
Nathan Adams <npadams21@gmail.com>
2025-03-21 06:51:52 +0000

    Delete an issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,10 +2,12 @@
 'use client';
 import { TrashIcon } from '@radix-ui/react-icons';
 import { AlertDialog, Button, Flex } from '@radix-ui/themes';
-import Link from 'next/link';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 const DeleteIssueButton = ({ issueId }: { issueId: number; }) => {
+  const router = useRouter();
   return (
     <AlertDialog.Root>
       <AlertDialog.Trigger>
@@ -16,7 +18,10 @@ const DeleteIssueButton = ({ issueId }: { issueId: number; }) => {
         <AlertDialog.Description>Are you sure you want to delte this issue. This action cannot be undone</AlertDialog.Description>
         <Flex className='mt-4' gap={'3'}>
           <AlertDialog.Cancel><Button variant='soft' color='gray'>Cancel</Button></AlertDialog.Cancel>
-          <AlertDialog.Action><Button color='red'>Delete Issue</Button></AlertDialog.Action>
+          <AlertDialog.Action><Button onClick={async () => {
+            await axios.delete('/api/issues/' + issueId);
+            router.push('/issues');
+          }} color='red'>Delete Issue</Button></AlertDialog.Action>
         </Flex>
       </AlertDialog.Content>
     </AlertDialog.Root>
