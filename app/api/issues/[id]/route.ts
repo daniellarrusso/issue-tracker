54257c87b95d94cb1d2da1c93d2d9ac8f252ce80
Nathan Adams <npadams21@gmail.com>
2025-03-24 11:20:43 +0000

    Add assignment of issues to API

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,16 +1,16 @@
 import authOptions from '@/app/auth/authOptions';
-import { issueSchema } from '@/app/validationSchemas';
+import { issueSchema, pathIssueSchema } from '@/app/validationSchemas';
 import { prisma } from '@/prisma/client';
+import { Issue } from '@prisma/client';
 import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string; }>; }) {
-  const session = await getServerSession(authOptions);
-  if (!session)
-    return NextResponse.json({}, { status: 401 });
+  // const session = await getServerSession(authOptions);
+  // if (!session) return NextResponse.json({}, { status: 401 });
 
-  const body = await request.json();
-  const validation = issueSchema.safeParse(body);
+  const body: Issue = await request.json();
+  const validation = pathIssueSchema.safeParse(body);
   const { id } = await params;
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
@@ -24,11 +24,22 @@ export async function PATCH(request: NextRequest, { params }: { params: Promise<
   if (!issue)
     return NextResponse.json({ error: 'Invalid Issue' }, { status: 404 });
 
+  const { assignedToUserId, description, title } = body;
+  if (assignedToUserId) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: assignedToUserId
+      }
+    });
+    if (!user) return NextResponse.json({ error: 'Invalid User' }, { status: 400 });
+  }
+
   const updatedIssue = await prisma.issue.update({
     where: { id: issue.id },
     data: {
-      title: body.title,
-      description: body.description
+      title,
+      description,
+      assignedToUserId
     }
   });
 
--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -2,5 +2,11 @@ import { z } from 'zod';
 
 export const issueSchema = z.object({
   title: z.string().min(1, 'Title is Required').max(255),
-  description: z.string().min(1, 'Description is Required')
+  description: z.string().min(1, 'Description is Required').max(65000)
+});
+
+export const pathIssueSchema = z.object({
+  title: z.string().min(1, 'Title is Required').max(255).optional(),
+  description: z.string().min(1, 'Description is Required').max(65000).optional(),
+  assignToUserId: z.string().max(255).nullable().optional()
 });
