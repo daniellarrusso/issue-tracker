2e11c09ce2d4eef92945d8b2ddf3db7587538d44
Nathan Adams <npadams21@gmail.com>
2025-03-25 16:23:07 +0000

    fix filtering bugs

--- app/issues/[id]/IssueStatusFilter.tsx
+++ app/issues/[id]/IssueStatusFilter.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { Status } from '@prisma/client';
 import { Select } from '@radix-ui/themes';
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 
 import React from 'react';
@@ -15,11 +15,16 @@ const statusFilters: { label: string; status?: Status; }[] = [
 
 const IssueStatusFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
-    <Select.Root onValueChange={(status) => {
-      const queryParams = status !== 'All' ? '?status=' + status : '';
-      router.push('/issues/list' + queryParams);
+    <Select.Root defaultValue={searchParams.get('status')!} onValueChange={(status) => {
+      const params = new URLSearchParams();
+      if (status) params.append('status', status);
+      if (searchParams.get('orderBy')) params.append('orderBy', searchParams.get('orderBy')!);
+
+      const query = params.size ? '?' + params.toString() : '';
+      router.push('/issues/list' + query);
     }}>
       <Select.Trigger placeholder='Apply Filter...'></Select.Trigger>
       <Select.Content>
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,20 +1,31 @@
 import { prisma } from '@/prisma/client';
 import { Table } from '@radix-ui/themes';
-import React from 'react';
+import React, { useState } from 'react';
 import IssueStatusBadge from '@/app/components/IssueStatusBadge';
 import delay from 'delay';
 import IssueActions from './IssueActions';
 import StyledLink from '@/app/components/StyledLink';
-import { Status } from '@prisma/client';
+import { Issue, Status } from '@prisma/client';
+import Link from 'next/link';
+import { FaArrowDown, FaArrowUp } from 'react-icons/fa';
 
-const IssuePage = async ({ searchParams }: { searchParams: Promise<{ status: Status; }>; }) => {
-  const { status } = await searchParams;
+const IssuePage = async ({ searchParams }: { searchParams: Promise<{ status: Status; orderBy: keyof Issue; direction: string; }>; }) => {
+  const params = await searchParams;
   const statuses = Object.values(Status);
-  const validStatus = statuses.includes(status) ? status : undefined;
+  const columns: { label: string, value: keyof Issue, className?: string; }[] = [
+    { label: 'Issue', value: 'title' },
+    { label: 'Status', value: 'status', className: 'hidden md:table-cell' },
+    { label: 'Created', value: 'createdAt', className: 'hidden md:table-cell' },
+  ];
+
+  const validStatus = statuses.includes(params.status) ? params.status : undefined;
+  const orderBy = columns.map(col => col.value).includes(params.orderBy) ? { [params.orderBy]: params.direction } : undefined;
+
   const issues = await prisma.issue.findMany({
     where: {
       status: validStatus
-    }
+    },
+    orderBy
   });
 
 
@@ -25,9 +36,15 @@ const IssuePage = async ({ searchParams }: { searchParams: Promise<{ status: Sta
       <Table.Root variant='surface'>
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+            {columns.map(col =>
+              <Table.ColumnHeaderCell className={col.className} key={col.value}>
+                <Link href={{
+                  query: { ...params, orderBy: col.value, direction: params.direction === 'asc' ? 'desc' : 'asc' }
+                }
+                }>{col.label}</Link>
+                {col.value === params.orderBy ? params.direction === 'asc' ? <FaArrowUp className='inline' /> : <FaArrowDown className='inline' /> : null}
+              </Table.ColumnHeaderCell>
+            )}
           </Table.Row>
         </Table.Header>
         <Table.Body>
